1ad64cfb3525ed576f043a043ef2532fb3e09a4a
David <dcounagogonzalez@danielcastelao.org>
2023-02-24 10:54:16 +0100

    Initial commit

---
+++ src/main/java/org/example/SocketCliente.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+
+public class SocketCliente {
+
+    public static void main(String[] args){
+        try{
+            System.out.println("Obteniendo factoria de sockets cliente");
+
+            SSLSocketFactory SocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+
+            System.out.println("Creando socket cliente");
+
+            SSLSocket clientSocket=(SSLSocket) SocketFactory.createSocket();
+
+            System.out.println("Estableciendo la conexión");
+
+            InetSocketAddress addr=new InetSocketAddress("localhost", 5555);
+            clientSocket.bind(addr);
+
+            OutputStream os=clientSocket.getOutputStream();
+
+            System.out.println("Enviando mensaje");
+
+            String mensaje="mensaje desde el cliente transmitido por SSL";
+            os.write(mensaje.getBytes());
+
+
+            System.out.println("Mensaje enviado");
+
+            System.out.println("Cerrando el socket cliente");
+
+            clientSocket.close();
+
+            System.out.println("Terminado");
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
