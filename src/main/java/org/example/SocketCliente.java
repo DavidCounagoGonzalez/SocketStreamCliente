76c82b3f94ebc3d020c944ba3fa1cb795a3a58cc
David <dcounagogonzalez@danielcastelao.org>
2023-03-10 09:23:26 +0100

    Lo haremos sin SSL

--- src/main/java/org/example/SocketCliente.java
+++ src/main/java/org/example/SocketCliente.java
@@ -1,48 +1,41 @@
 package org.example;
 
-import javax.net.ssl.SSLSocket;
-import javax.net.ssl.SSLSocketFactory;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-
+import java.net.Socket;
 
 public class SocketCliente {
 
     public static void main(String[] args){
         try{
-            System.out.println("Obteniendo factoria de sockets cliente");
-
-            SSLSocketFactory SocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-
             System.out.println("Creando socket cliente");
+            Socket clienteSocket=new Socket();
+            System.out.println("Estableciendo la conexi�n");
 
-            SSLSocket clientSocket=(SSLSocket) SocketFactory.createSocket();
+            InetSocketAddress addr=new InetSocketAddress("localhost",5555);
+            clienteSocket.connect(addr);
 
-            System.out.println("Estableciendo la conexión");
-
-            InetSocketAddress addr=new InetSocketAddress("localhost", 5555);
-            clientSocket.bind(addr);
-
-            OutputStream os=clientSocket.getOutputStream();
+            InputStream is = clienteSocket.getInputStream();
+            OutputStream os= clienteSocket.getOutputStream();
 
             System.out.println("Enviando mensaje");
 
-            String mensaje="mensaje desde el cliente transmitido por SSL";
+            String mensaje="mensaje desde el cliente";
             os.write(mensaje.getBytes());
-
 
             System.out.println("Mensaje enviado");
 
             System.out.println("Cerrando el socket cliente");
 
-            clientSocket.close();
+            clienteSocket.close();
 
             System.out.println("Terminado");
 
-        }catch(Exception e){
+        }catch (IOException e) {
             e.printStackTrace();
         }
     }
-    //https://stackoverflow.com/questions/8901637/re-use-a-java-socket-client-once-again
 }
 
